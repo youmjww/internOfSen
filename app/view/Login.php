c5426f7b50a93f62f1cc2566f514a3436d0674f4
youmjww <b4234@oic.jp>
2017-06-16 01:39:37 +0900

    ログイン画面の見てくれだけを作った
    説明
    見てくれだけ作った
    中身はまだない

---
+++ app/view/Login.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Login.php
+ *
+ *  @author     {$author}
+ *  @package    Sample
+ */
+
+/**
+ *  login view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sample
+ */
+class Sample_View_Login extends Sample_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
---
+++ template/ja_JP/login.tpl
@@ -0,0 +1,15 @@
+<form action="." method="post">
+  <table border="0">
+    <tr>
+      <td>メールアドレス</td>
+      <td><input type="text" name="mailaddress" value=""></td>
+    </tr>
+    <tr>
+      <td>パスワード</td>
+      <td><input type="password" name="password" value=""></td>
+    </tr>
+  </table>
+  <p>
+  <input type="submit" name="action_login_do" value="ログイン">
+  </p>
+</form>
