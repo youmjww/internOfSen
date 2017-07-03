434a78d7c10c15ec98959f07b573b7e08699520b
youmjww <b4234@oic.jp>
2017-07-03 15:25:56 +0900

    action定義の簡略化
    説明
    コントローラーに簡略化したアクションの定義を記述（チュートリアルを読んでいたらなんやったらコントローラーに記述しなくても良さそう・・・）

--- app/Sample_Controller.php
+++ app/Sample_Controller.php
@@ -60,18 +60,7 @@ class Sample_Controller extends Ethna_Controller
      *  @var    array   action definition.
      */
     protected $action = array(
-        /*
-         *  TODO: write action definition here.
-         *
-         *  Example:
-         *
-         *  'index'     => array(
-         *      'form_name' => 'Sample_Form_SomeAction',
-         *      'form_path' => 'Some/Action.php',
-         *      'class_name' => 'Sample_Action_SomeAction',
-         *      'class_path' => 'Some/Action.php',
-         *  ),
-         */
+        'some_action' => array(),
     );
 
     /**
