a3985b6bd12c4c70ec4309b05765b4739ef0f081
youmjww <b4234@oic.jp>
2017-07-03 18:07:43 +0900

    wip

---
+++ app/view/ReadFile.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  ReadFile.php
+ *
+ *  @author     {$author}
+ *  @package    Sample
+ */
+
+/**
+ *  readFile view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sample
+ */
+class Sample_View_ReadFile extends Sample_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
---
+++ template/ja_JP/readFile.tpl
@@ -0,0 +1,7 @@
+  <h2>New Template</h2>
+  <p>read file</p>
+
+  <form method="post" enctype="multipart/form-data">
+    <input type="file" name="sample_file">
+    <input type="submit" name="action_test">
+  </form>
