853b1e8c6b6bc2930bf86079179b393558605d2d
youmjww <b4234@oic.jp>
2017-07-10 14:17:56 +0900

    フィルター処理を学習
    説明
    フィルターを実装すると小文字を大文字に変換したり、全角を半角にしたり出来ることを学習した.

--- app/action/Validate.php
+++ app/action/Validate.php
@@ -25,9 +25,22 @@ class Sample_Form_Validate extends Sample_ActionForm
            'min' => '4',
            'max' => '8',
            'required'   => true,
+           'filter' => 'numeric_zentohan,alphabet_zentohan,ltrim,rtrim,ntrim',  //全角を半角に変換して、左右の空白を削除してる
            'type'   => VAR_TYPE_STRING,
        ],
     );
+
+    /**
+    * フォーム値変換フィルタ: サンプル
+    *
+    * @access protected
+    * @param mixed $value フォーム値
+    * @return mixed 変換結果
+    */
+    function _filter_sample($value)
+    {
+        return strtoupper($value);
+    }
 }
 
 class Sample_Action_Validate extends Sample_ActionClass
@@ -35,7 +48,8 @@ class Sample_Action_Validate extends Sample_ActionClass
     public function prepare()
     {
         if ($this->af->validate() > 0) {
-            $error = 'メアドがなんかおかしいです';
+
+            var_dump($this->af->validate('sample'));
             $this->af->set('error', $error);
             return null;
         }
