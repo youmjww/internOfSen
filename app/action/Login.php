88073437783d639a2d7f3c57fb3268b91c3b8f1c
youmjww <youmjww@gmail.com>
2017-07-19 15:04:13 +0900

    selectを配列でアクセス出来たーーー！！！！！！！

--- app/action/Login.php
+++ app/action/Login.php
@@ -28,64 +28,24 @@ class Sample_Form_Login extends Sample_ActionForm
         ),
     );
 
-    /**
-     *  Form input value convert filter : sample
-     *
-     *  @access protected
-     *  @param  mixed   $value  Form Input Value
-     *  @return mixed           Converted result.
-     */
-    /*
-    protected function _filter_sample($value)
-    {
-        //  convert to upper case.
-        return strtoupper($value);
-    }
-    */
 }
 
-/**
- *  login action implementation.
- *
- *  @author     {$author}
- *  @access     public
- *  @package    Sample
- */
 class Sample_Action_Login extends Sample_ActionClass
 {
-    /**
-     *  preprocess of login Action.
-     *
-     *  @access public
-     *  @return string    forward name(null: success.
-     *                                false: in case you want to exit.)
-     */
     public function prepare()
     {
         require_once('adodb5/adodb.inc.php');
 
         $db = $this->backend->getDB();
-        $rs = $db->query('SELECT * FROM test');
-        // $db =& $this->backend->getDB();
-        // $sql = "SELECT * FROM test";
-        // $result =& $db->query($sql);
-        // $i = 0;
-        // while ($data[$i] = $result->fetchRow()) {
-        //     $i++;
-        // }
+        $rs = $db->query('SELECT * FROM users;');
 
-        //var_dump($result);
+
+        print_r($rs->getRows());
 
         return null;
     }
 
-    /**
-     *  login action implementation.
-     *
-     *  @access public
-     *  @return string  forward name.
-     */
-    public function perform()
+   public function perform()
     {
         return 'login';
     }
