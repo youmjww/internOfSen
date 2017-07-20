fc7830baf702fecaf472e32cb472c82d3852c017
youmjww <youmjww@gmail.com>
2017-07-20 16:51:57 +0900

    ユーザー追加画面を作成
    説明
    ユーザーを追加するための画面を作った。ちなみに、ログインしていないとここのページにはアクセス出来ないです。

--- app/action/Adduser/Do.php
+++ app/action/Adduser/Do.php
@@ -5,38 +5,70 @@ class Sample_Form_AdduserDo extends Sample_ActionForm
     public $form = array(
         'username'  =>  [
             'name'  =>  'ユーザーネーム',
-            'required'  =>  true,
-            'type'      =>  VAR_TYPE_STRING
-         ],
+            'required' => true,
+            'type' => VAR_TYPE_STRING,
+        ],
+
         'password' => [
-            'name'  =>  'パスワード',
-            'required'  =>  true,
-            'type'  =>  VAR_TYPE_STRING,
+            'name' => 'パスワード',
+            'required' => true,
+            'type' => VAR_TYPE_STRING,
         ],
 
         'mailaddres' => [
-            'name'  =>  'メールアドレス',
-            'required'  =>  true,
-            'type'  =>  VAR_TYPE_STRING,
+            'mailaddres' => 'メールアドレス',
+            'required' => true,
+            'type' => VAR_TYPE_STRING,
         ],
-
-   );
+    );
 }
-
 
 class Sample_Action_AdduserDo extends Sample_ActionClass
 {
+
+    private function setUser()
+    {
+        // フォームのデータを変数に持っておいてescapeする
+        $username = $this->af->get('username');
+        $password = $this->af->get('password');
+        $mailaddres = $this->af->get('mailaddres');
+        $escapeUsername = pg_escape_string($username);
+        $escapePassword = pg_escape_string($password);
+        $escapeMailaddres = pg_escape_string($mailaddres);
+
+        require_once('adodb5/adodb.inc.php');
+
+        $db = $this->backend->getDB();
+
+        // dbに値をセットする
+        $db->query("INSERT INTO users(id,name,password,mailaddres) values (nextval('userId'),'$escapeUsername','$escapePassword','$escapeMailaddres');");
+
+    }
+
     public function prepare()
     {
 
+        // 必須入力が入力されていなければ
         if ($this->af->validate() > 0) {
-            return 'endregist';
+            return 'addUser';
         }
+
+        $this->setUser();
+
         return null;
     }
 
     public function perform()
     {
+        $username = $this->af->get('username');
+        $this->af->set('setResult', "ユーザーの追加が完了しました。$username");
         return 'addUser';
+    }
+
+    function authenticate()
+    {
+        if ( !$this->session->isStart() ) {
+           return 'login';
+        }
     }
 }
--- template/ja_JP/addUser.tpl
+++ template/ja_JP/addUser.tpl
@@ -1,21 +1,30 @@
 <h2>ユーザーの新規追加</h2>
 
 <form action="." method="post">
-  <table border="0">
-    <tr>
-      <td>user name</td>
-      <td><input type="text" name="username" value=""></td>
+    {if count($errors)}
+        <ul>
+            {foreach from=$errors item=error}
+                <li>{$error}</li>
+            {/foreach}
+        </ul>
+    {/if}
 
+
+  <table border="0">
+    {$form.setResult}
+    <tr>
+      <td>ユーザー名</td>
+      <td><input type="text" name="username" value=""></td>
     </tr>
     <tr>
-     <td>password</td>
+        <td>メールアドレス</td>
+        <td><input type="text" name="mailaddres" value=""></td>
+    </tr>
+    <tr>
+     <td>パスワード</td>
          <td><input type="password" name="password" value=""></td>
     </tr>
-    <tr>
-        <td>mailaddress</td>
-        <td><input type="mailaddres" name="mailaddres" value="{$from.mailaddress}"></td>
-    </tr>
-  </table>
+   </table>
   <p>
   <input type="submit" name="action_adduser_do" value="登録">
   </p>
