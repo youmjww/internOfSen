75d9f2231433c3147311d28c1da936dd3bc0ccc5
youmjww <youmjww@gmail.com>
2017-08-23 17:36:29 +0900

    グループ機能の作成
    説明
    今までは、一覧でしか写真を確認できなかったが、グループで確認できるようにしました。
    
    苦労したところ
    一覧からグループ名を変更する時に、選択されているグループがいちいちallを選択される不具合で苦労しました。

--- app/action/ViewAllPhoto.php
+++ app/action/ViewAllPhoto.php
@@ -8,9 +8,17 @@ class Sample_Form_ViewAllPhoto extends Sample_ActionForm
             'type' => VAR_TYPE_STRING,
             'max' => 80,
         ),
- 
+
         'name' => array(
             'type' => VAR_TYPE_STRING,
+        ),
+
+        'group' => array(
+            'type' => VAR_TYPE_STRING,
+        ),
+        'newGroupName' => array(
+            'type' => VAR_TYPE_STRING,
+            'required' => true,
         ),
     );
 }
@@ -26,6 +34,37 @@ class Sample_Action_ViewAllPhoto extends Sample_ActionClass
         $db->query("update photos set memo = '$escapeMemo' where filepath = '$filepath';");
 
     }
+
+    private function setGroupName ($newGroupName, $filepath)
+    {
+        $escapGroupName = pg_escape_string($newGroupName);
+        require_once('adodb5/adodb.inc.php');
+        $db = $this->backend->getDB();
+        $db->query("update photos set goulp = '$escapGroupName' where filepath = '$filepath';");
+
+    }
+
+    private function groupFilter($group, $allPhoto)
+    {
+        // phpのデフォルトが参照渡しか値渡しかわからないけど、怖いから配列をコピーしておく
+        $copyAllPhoto = $allPhoto;
+
+        if ($group === NULL || $group === 'all')
+        {
+            return $copyAllPhoto;
+        }
+
+        $result = [];
+        foreach ($copyAllPhoto as $photo)
+        {
+            if($photo['goulp'] === $group)
+            {
+                array_push($result, $photo);
+            }
+        }
+        return $result;
+    }
+
     public function prepare()
     {
         return null;
@@ -33,18 +72,37 @@ class Sample_Action_ViewAllPhoto extends Sample_ActionClass
 
     public function perform()
     {
+        $selectGroup = $this->af->get('group');
+
         if ( $this->af->get('memo') != NULL)
         {
             $this->setMemo ($this->af->get('memo'), $this->af->get('name'));
+        }
+
+        if ($this->af->get('newGroupName') != NULL)
+        {
+           $this->setGroupName ($this->af->get('newGroupName'), $this->af->get('name'));
+            $selectGroup = $this->af->get('newGroupName');
         }
 
         $userId = $this->session->get('userName')['userId'];
 
         require_once('adodb5/adodb.inc.php');
         $db = $this->backend->getDB();
-        $allPhoto = $db->query("select filepath, memo from photos where userid = '$userId'  order by timestamp;")->getRows(['']);
+
+        // 写真を表示する用
+        $allPhoto = $db->query("select filepath, memo, goulp from photos where userid = '$userId'  order by timestamp;")->getRows(['']);
+        $allPhoto = $this->groupFilter($selectGroup, $allPhoto);
         $this->af->setApp( 'allPhoto', $allPhoto );
 
+        // プルダウン用
+        $group = $db->query("select goulp from photos where userid = '$userId'")->getRows(['goulp']);
+        $preGroup = ['all'];
+        foreach($group as $value){
+            array_push ( $preGroup, $value['goulp'] );
+        }
+        $this->af->setApp('nowSelect', $selectGroup);
+        $this->af->setApp('groupList', array_unique($preGroup));
         return 'viewAllPhoto';
     }
 
--- template/ja_JP/viewAllPhoto.tpl
+++ template/ja_JP/viewAllPhoto.tpl
@@ -1,9 +1,31 @@
+<form action="." method="post" name="groupSelect">
+    <select name="group">
+        {foreach from=$app.groupList item=list}
+            {if $list eq $app.nowSelect}
+                <option value="{$list}" selected>{$list}</option>
+            {else}
+                <option value="{$list}">{$list}</option>
+            {/if}
+        {/foreach}
+    </select>
+        <input type="submit" name="action_viewAllPhoto" value="グループを選択"></input>
+</form>
+<br>
+
+
 {foreach from=$app.allPhoto item=photo}
         <form action="." method="post">
 
             <img src="{$photo.filepath}" style="width:50%" data-action="zoom"><br>
             <p>{$photo.memo}</p>
             <textarea type="text" name="memo" maxlength="80"  placeholder="メモ"></textarea>
+            <input type="hidden" name="name" value="{$photo.filepath}"></input>
+            <input type="submit" name="action_ViewAllPhoto" value="送信"></input>
+            <input type="hidden" name="group" value="{$app.nowSelect}"></input>
+
+        </form>
+        <form action="." method="post">
+            <input type="text" name="newGroupName" maxlength="20"  placeholder="グループ"></textarea>
             <input type="hidden" name="name" value="{$photo.filepath}"></input>
             <input type="submit" name="action_ViewAllPhoto" value="送信"></input>
         </form>
